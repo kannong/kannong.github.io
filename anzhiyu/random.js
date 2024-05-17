a93c8ef0fee65ea1f7328a817dba44c9a6ef1fff
kannong <2637053537@qq.com>
2024-05-17 14:44:11 +0800

    Site updated: 2024-05-17 14:44:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/01/hello-world/","2024/05/03/music/","2024/05/14/xiye-TerenceLam/","2024/05/03/music_test/","2024/05/01/搭建我的博客/","2024/05/03/xiye2024/","2024/05/14/xiye2020s/"];function toRandomPost(){
+var posts=["2024/05/01/hello-world/","2024/05/03/music/","2024/05/03/music_test/","2024/05/14/xiye-TerenceLam/","2024/05/01/搭建我的博客/","2024/05/03/xiye2024/","2024/05/14/xiye2020s/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
