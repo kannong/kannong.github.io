26631f7bea3281eab493a040f9077df9d7fce7b3
kannong <2637053537@qq.com>
2025-07-24 15:43:12 +0800

    Site updated: 2025-07-24 15:42:56

---
+++ js/baiduanalyse.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?535ea93640679a99042a3e9521226324";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
